8333440e7e5b4f0776b23aca952a66aab8c1221e
LexaSolovey <xoxach5@gmail.com>
2017-09-18 13:20:53 +0300

    finished

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function multiply(first, second) {
-  // your solution
-}
+    first = first.split('').reverse();
+    second = second.split('').reverse();
+
+    let resultInArray = [];
+    first.map((itemOfFirst, indexOfFirst) => {
+      second.map((itemOfSecond, indexOfSecond) => {
+        const product = itemOfFirst * itemOfSecond + (resultInArray[indexOfFirst + indexOfSecond] || 0);
+        let decades = 0;
+        product < 9
+          ? resultInArray[indexOfFirst + indexOfSecond] = product
+          : (
+            decades = Math.floor(product/10),
+            resultInArray[indexOfFirst + indexOfSecond] = product - decades*10,
+            resultInArray[indexOfFirst + indexOfSecond + 1] = (resultInArray[indexOfFirst + indexOfSecond + 1] || 0) + decades
+          );
+      });
+    });
+    return resultInArray.reverse().join('');
+};
